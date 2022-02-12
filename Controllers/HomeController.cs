b705db4f5336262ed517e3b0f65f87c469bceec7
Gouldking <59857002+Gouldking@users.noreply.github.com>
2022-02-11 17:10:26 -0700

    06 - Lambda

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,27 +11,12 @@ namespace Bookstore.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        private BookstoreContext context { get; set; }
+        public HomeController(BookstoreContext temp) => context = temp;
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
