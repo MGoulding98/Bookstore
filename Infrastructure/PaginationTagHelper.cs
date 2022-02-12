a9a5fd1ee53831e85568ae58d29e577b08bf5596
Gouldking <59857002+Gouldking@users.noreply.github.com>
2022-02-12 10:44:58 -0700

    Phase02 - 04

---
+++ Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PaginationTagHelper : TagHelper
+    {
+        // Dynamically create page links
+    }
+}
