124685815c0c39b3ae2e3a87b12062dbd3443af3
Gouldking <59857002+Gouldking@users.noreply.github.com>
2022-02-20 11:59:30 -0700

    04 - 05

--- Pages/Purchase.cshtml.cs
+++ Pages/Purchase.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bookstore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,12 +10,26 @@ namespace Bookstore.Pages
 {
     public class PurchaseModel : PageModel
     {
+        private IBookstoreRepository repo { get; set; }
+
+        public PurchaseModel (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+        public Cart cart { get; set; }
+
         public void OnGet()
         {
         }
 
-        public void OnPost()
+        public IActionResult OnPost(int bookId)
         {
+            Book b = repo.Books.FirstOrDefault(x => x.BookId == bookId);
+
+            cart = new Cart();
+            cart.AddItem(b, 1);
+
+            return RedirectToPage();
         }
     }
 }
