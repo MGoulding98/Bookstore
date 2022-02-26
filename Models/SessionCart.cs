ebaa87d9c267aa712f4e0e151b5d564f013a009e
Gouldking <59857002+Gouldking@users.noreply.github.com>
2022-02-26 12:13:48 -0700

    05 - 02

---
+++ Models/SessionCart.cs
@@ -0,0 +1,34 @@
+﻿using Bookstore.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    public class SessionCart : Cart
+    {
+        [JsonIgnore]
+        public ISession Session { get; set; }
+
+        public override void AddItem(Book book, int qty)
+        {
+            base.AddItem(book, qty);
+            Session.SetJson("Cart", this);
+        }
+
+        public override void RemoveItem(Book book)
+        {
+            base.RemoveItem(book);
+            Session.SetJson("Cart", this);
+        }
+
+        public override void ClearBasket()
+        {
+            base.ClearBasket();
+            Session.Remove("Cart");
+        }
+    }
+}
